163e53aeac3b78be64024965d85391daeff4d4a4
kanjirubridget <98301637+kanjirubridget@users.noreply.github.com>
2022-07-24 16:38:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let Bridget = 'amazing';
+        if(Bridget==='amazing')alert('Bridget is amazing');
+        Bridget = 'Beautiful';
+        if(Bridget==='amazing')alert('Bridget is amazing');
+      1+4;
+      console.log(1+4);
+      9*4;
+      console.log(9*4);
+      36/4;
+      console.log(36/4);
+      2/2*6-1;
+      console.log(2/2*6-1);
+/*let firstName = 'Bridget';
+console.log(firstName)
+let middleName = 'Kanjiru'
+console.log(middleName)
+*/
+/*let PI = 3.14
+console.log(PI)
+let my_first_job = 'marketing'
+console.log(
+  my_first_job
+)
+const country = 'Kenya';
+let population = '50 million';
+const continent = 'Africa';
+console.log(country);
+console.log(continent);
+console.log(population);
+//let age= 23;
+//if(firstName === 'Bridget') alert('Bridget is the best');
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+/*console.log(firstName = 'Bridget')
+console.log(typeof firstName);
+console.log(typeof javaScriptIsFun)
+
+let age = 21
+age =25;
+
+console.log(age);
+const birthYear = 1991;
+console.log(birthYear);
+
+var number = 20;
+number =22;
+console.log(number);
+
+const surname = 'Mwithalii'
+console.log(surname);
+*/
+/*const now =2022;
+console.log(now-2000);
+
+age = now -2011;
+console.log(age);
+
+/*let x =10+5;
+console.log(x+=10);
+
+console.log(x-=5);
+console.log(2**3);
+*/
+
+/*const firstName= 'Bridget';
+const middleName = 'Kanjiru';
+
+console.log(firstName+" "+middleName);
+console.log(firstName+middleName);
+
+let x= 10;
+x++;
+console.log(x);
+console.log(x--);
+console.log(x-1);
+
+let y = 9;
+console.log(y--);
+
+console.log(x>y);
+console.log(x,y);
+console.log(now-x > now-y);
+console.log(now>=2011);
+
+let island = 'Kenya';
+const language = 'Swahili';
+
+console.log(island);
+console.log(language);
+
+population = '52000000';
+console.log(population);
+console.log(population/2);
+console.log(population+1);
+*/
+
+let kenyaPopulation = 52000000;
+let finlandPopulation = 6000000;
+
+console.log(kenyaPopulation>finlandPopulation);
+console.log(finlandPopulation-kenyaPopulation);
+
+ let y = 9;
+ console.log(--y);
+
+
+
+
+
